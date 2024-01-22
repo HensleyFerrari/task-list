d0978cdbf6cda4ac76fc380f1a34ebb0d25d76b3
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-01-22 08:01:15 -0300

    feat: added route buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Task List | PHP</title>
     @yield('styles')
 </head>
 
-<body>
+<body class="container mx-auto">
     <h1>@yield('title')</h1>
     <div>
         @if (session()->has('success'))
